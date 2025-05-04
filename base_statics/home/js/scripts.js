2e7c39ab848653375f0a56b35a9c80d725e1697f
Jhulyz <marcusvrds07@gmail.com>
2025-05-04 20:32:05 -0300

    erro do carrousel arrumado

--- base_statics/home/js/scripts.js
+++ base_statics/home/js/scripts.js
@@ -5,18 +5,18 @@ if (customSelect) {
   var arrow = customSelect.querySelector(".arrow");
   var options = customSelect.querySelector(".options");
   var optionItems = options.querySelectorAll(".option");
-  
+
   selectedOption.addEventListener("click", function () {
     options.classList.toggle("open");
     arrow.classList.toggle("rotate");
   });
-  
+
   optionItems.forEach(function (item) {
     item.addEventListener("click", function (event) {
       selectText.firstChild.textContent = event.target.textContent;
     });   
   });
-  
+
   document.addEventListener("click", function (event) {
     if (!customSelect.contains(event.target)) {
       options.classList.remove("open");
@@ -45,18 +45,18 @@ function prevSlide() {
   updateCarroussel();
 }
 
-setInterval(nextSlide, Listener("resize", updateCarroussel));
-
+window.addEventListener("resize", updateCarroussel);
+setInterval(nextSlide, 15000);
 
 const navbar = document.getElementById('navbar');
-    const trigger = document.getElementById('carroussel');
+const trigger = document.getElementById('carroussel');
 
-    window.addEventListener('scroll', () => {
-      const triggerBottom = trigger.getBoundingClientRect().bottom;
+window.addEventListener('scroll', () => {
+  const triggerBottom = trigger.getBoundingClientRect().bottom;
 
-      if (triggerBottom <= 90) {
-        navbar.classList.add('scrolled');
-      } else {
-        navbar.classList.remove('scrolled');
-      }
-    });
+  if (triggerBottom <= 90) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
