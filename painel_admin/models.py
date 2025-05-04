1b6e876b392f0c2c2d71db4da0ac67ff47235bb5
Jhulyz <marcusvrds07@gmail.com>
2025-05-04 00:00:29 -0300

    Index html do admin

--- painel_admin/models.py
+++ painel_admin/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+def user_profile_photo_path(instance, filename):
+    return f'usuarios/{instance.user.id}/{filename}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to=user_profile_photo_path, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
