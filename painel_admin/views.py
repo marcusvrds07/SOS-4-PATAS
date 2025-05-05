3b708bc7c25809d3b7b983a63980dbee06872e14
Jhulyz <marcusvrds07@gmail.com>
2025-05-05 00:48:02 -0300

    adicionado contagem de voluntarios, animais disponiveis e adotados na admin

--- painel_admin/views.py
+++ painel_admin/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from animais.models import Animais
+from django.contrib.auth.models import User
+from django.contrib.admin.sites import site
 
-# Create your views here.
+def dashboard_admin(request):
+    animais = Animais.objects.filter(disponivel_para_adocao=True).count()
+    voluntarios = User.objects.filter(is_active=True).count()
+    adocoes = 0
 
-def index(request):
-    return render(request, 'index.html')
+    return render(request, 'admin/index.html', {
+        'app_list': site.get_app_list(request),
+        'animais_disponiveis': animais,
+        'voluntarios_ativos': voluntarios,
+        'adocoes_concluidas': adocoes
+    })
+
--- sos4patas/urls.py
+++ sos4patas/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from painel_admin.views import dashboard_admin
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -30,6 +31,7 @@ urlpatterns = [
     path('admin/redefinir-senha/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('admin/senha-redefinida/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
+    path('admin/', dashboard_admin, name='admin-index'),
     path('admin/', admin.site.urls),
 ]
 
